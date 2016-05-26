4d97d97c45ac383c984ae38510ccc2a2d647d4d5
Pheophorbide <daria.paraonova@gmail.com>
2016-05-26 15:54:38 +0300

    Доработки

--- build/js/check.js
+++ build/js/check.js
@@ -1,22 +1,22 @@
-var getMessage = function (a, b) {
+  var getMessage = function (a, b) {
   if (typeof a == 'boolean') {
     if (a == true) {
-      return ('Я попал в ' + b);
+      return 'Я попал в ' + b;
     }
     else {
-      return ('Я никуда не попал');
+      return 'Я никуда не попал';
     }
   }
   else if (typeof a == 'number') {
-    return ('Я прыгнул на ' + a * 100 + ' сантиметров');
+    return 'Я прыгнул на ' + a * 100 + ' сантиметров';
 
   }
   else if (typeof a == 'object' && typeof b == 'object') {
-    var lenght = 0;
+    var length = 0;
     for (var i = 0; i < a.length && i < b.length; i++) {
-      lenght += a[i] * b[i];
+      length += a[i] * b[i];
     }
-    return ('Я прошёл ' + lenght + ' метров');
+    return 'Я прошёл ' + length + ' метров';
   }
   else if (typeof a == 'object') {
 
@@ -24,8 +24,6 @@ var getMessage = function (a, b) {
     for (var i = 0; i < a.length; i++) {
       sum += a[i];
     }
-    return ('Я прошёл ' + sum + ' шагов');
+    return 'Я прошёл ' + sum + ' шагов';
   }
 };
-getMessage();
-
