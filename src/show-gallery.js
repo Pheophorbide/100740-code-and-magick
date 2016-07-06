eb88880f035e8f91a138c23a75f8328f44b9cdcc
Pheophorbide <daria.paraonova@gmail.com>
2016-07-06 13:17:58 +0300

    Переписала галерею с прототипами

--- src/show-gallery.js
+++ src/show-gallery.js
@@ -1,11 +1,13 @@
 'use strict';
 
-var gallery = require('./gallery');
+var Gallery = require('./gallery');
 
 (function() {
   var pictures = document.querySelectorAll('.photogallery-image img');
   var galleryContainer = document.querySelector('.photogallery');
   var picturesArr = Array.prototype.slice.call(pictures);
+  var galleryOverlayContainer = document.querySelector('.overlay-gallery');
+  var galleryElement = new Gallery(galleryOverlayContainer);
   var picturesArrSrc = picturesArr.map(function(pic) {
     return pic.src.replace(location.origin, '');
   });
@@ -18,14 +20,12 @@ var gallery = require('./gallery');
       currentNumber = activeNumber + 1;
       var hash = '/img/screenshots/' + currentNumber + '.png';
       location.hash = 'photo' + hash;
-      gallery.showGallery(activeNumber);
+      galleryElement.showGallery(activeNumber);
     }
     return false;
   });
 
-  gallery.savePictures(picturesArrSrc);
-  gallery.onHashChange();
-  window.addEventListener('hashchange', gallery.onHashChange);
-
-
+  galleryElement.savePictures(picturesArrSrc);
+  galleryElement.onHashChange();
+  window.addEventListener('hashchange', galleryElement.onHashChange);
 })();
